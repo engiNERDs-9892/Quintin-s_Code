745f6724cf2e437ef45707a2d70bff3595fa4bd8
enginerds_tccs <engiNERDs@tccs.org>
2024-03-27 16:01:23 -0700

    worked on code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Power_Auto.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+
+@TeleOp(group = "drive")
+//@Disabled
+public class Movement_Based_On_Power
